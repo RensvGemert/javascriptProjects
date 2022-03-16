4a8ffa2ca9ce280deae757afd4dd4680a40282d5
Rens Gemert <461328@student.fontys.nl>
2022-03-16 15:25:43 +0100

    Removed unused comments

--- FreeCodeCampOrgJsCourse/script.js
+++ FreeCodeCampOrgJsCourse/script.js
@@ -1,29 +1,4 @@
-// Practise 1:
-// document.getElementById("count-element").innerText = 3;
-
-
-// Practise 2:
-// let count = 1 + 1;
-// console.log(count);
-
-
-// Practise 3:
-// let myAge = 23;
-// let humanDogRatio = 7;
-// let myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge)
-
-
-// Practise 4:
-// let bonusPoints = 50;
-// bonusPoints = bonusPoints + 50;
-// console.log(bonusPoints);
-// bonusPoints = bonusPoints - 75;
-// console.log(bonusPoints);
-// bonusPoints = bonusPoints + 45;
-// console.log(bonusPoints);
-
-// Practise 5 onclick():
+// Practise onclick():
 let count = 0;
 
 function increment() {
