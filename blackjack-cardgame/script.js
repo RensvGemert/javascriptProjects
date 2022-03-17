ef85ccf43f51aa7a8d84ebd00430fcc9af140a11
Rens Gemert <461328@student.fontys.nl>
2022-03-17 16:53:28 +0100

    updated js projects

--- blackjack-cardgame/script.js
+++ blackjack-cardgame/script.js
@@ -1,20 +1,44 @@
 let firstCard = 10;
-let secondCard = 12;
+let secondCard = 5;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el");
+
 
 function startGame() {
-    
+    renderGame();
+}
+
+function renderGame() {   
+
+    cardsEl.textContent = "Cards: " + cards[0] + " + " + cards[1];
+    sumEl.textContent = "Sum: " + sum;
+
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
         message = "You've got BlackJack!";
+        alert(message);
         hasBlackJack = true;
     } else {
         message = "You're out of the game";
         isAlive = false;
     }
+    messageEl.textContent = message;
 }
+
+
+function newCard() {
+    let card = 6;
+    cards.push(card);
+    sum += card;
+    renderGame();
+}
+
+
---
+++ messaging-app/script.js
@@ -0,0 +1,10 @@
+let messages = [
+    "Hello!",
+    "How are you?",
+    "All good. Been working on this array project",
+    "Same here!",
+    "Great!"
+];
+
+console.log(messages);
+
