1bd30373daefc291e64ab05d54407dc0e79a42bd
Rens Gemert <461328@student.fontys.nl>
2022-03-17 12:09:41 +0100

    added new practise, refactored css files

--- FreeCodeCampOrgJsCourse/script.js
+++ FreeCodeCampOrgJsCourse/script.js
@@ -6,3 +6,21 @@ let greeting = "Welcome back ";
 welcomeEl.innerText = greeting + name;
 
 welcomeEl.innerText += "👋";
+
+// let myPoints = 3;
+
+// function add3Points() {
+//     myPoints += 3;
+// }
+
+// function remove1Point() {
+//     myPoints -= 1;
+// }
+
+// add3Points();
+// add3Points();
+// add3Points();
+// remove1Point();
+// remove1Point();
+
+// console.log(myPoints);
